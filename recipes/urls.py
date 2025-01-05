6e0489bf48b47ec0ec0f6d605dcd175b7395fcb6
Rhea Le <rheangocle@gmail.com>
2025-01-05 01:38:38 -0500

    added viewsets and urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,21 @@
 from rest_framework.routers import DefaultRouter
-from .views import RecipeViewSet, IngredientViewSet
+from .views import (
+    RecipeViewSet,
+    IngredientViewSet,
+    TagViewSet,
+    FeedbackViewSet,
+    InventoryViewSet,
+    CategoryViewSet,
+    UserProfileViewSet
+)
 
 router = DefaultRouter()
 router.register(r'ingredients', IngredientViewSet, basename='ingredient')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'feedback', FeedbackViewSet, basename='feedback')
+router.register(r'inventory', InventoryViewSet, basename='inventory')
+router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'users', UserProfileViewSet, basename='users')
 
 urlpatterns = router.urls
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,63 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Recipe, Ingredient
-from .serializers import RecipeSerializer, IngredientSerializer
+from recipes.models import Recipe, Ingredient, Tag, Inventory, Feedback, Category, UserProfile
+from .serializers import RecipeSerializer, IngredientSerializer, TagSerializer, InventorySerializer, FeedbackSerializer, CategorySerializer, UserProfileSerializer
+
+
+class RecipePagination(PageNumberPagination):
+    page_size = 10
 
 class IngredientViewSet(viewsets.ModelViewSet):
-    queryset = Ingredient.objects.all() # pylint: disable=no-member
+    queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category']
+    filterset_fields = ['category__id']
     
 class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all() # pylint: disable=no-member
+    queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['tags', 'cuisine']
+    pagination_class = RecipePagination
+    
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    
+class InventoryViewSet(viewsets.ModelViewSet):
+    serializer_class = InventorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ingredient__name', 'quantity']
+    
+    def get_queryset(self):
+        return Inventory.objects.filter(user=self.request.user)
+    
+class FeedbackViewSet(viewsets.ModelViewSet):
+    serializer_class = FeedbackSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['recipe']
+    
+    def get_queryset(self):
+        return Feedback.objects.filter(user=self.request.user)
+    
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAdminUser]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return UserProfile.objects.filter(user=self.request.user)
+    
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    
